cb632340967a82a859eae1fe2dac7fb14bd5ffde
sumanth nagulvancha <sumanthrao1997@gmail.com>
2023-03-20 11:37:09 +0100

    pybind with Track rgbd overload

--- python_wrapper/orb_slam3_pybind.cpp
+++ python_wrapper/orb_slam3_pybind.cpp
@@ -5,6 +5,7 @@
 #include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
@@ -12,7 +13,9 @@
 #include <cstdint>
 #include <iostream>
 #include <iterator>
+#include <mutex>
 #include <string>
+#include <vector>
 
 #include <opencv2/core/eigen.hpp>
 #include <opencv2/core/mat.hpp>
@@ -21,15 +24,15 @@
 #include <opencv2/opencv.hpp>
 
 //#include "ndarray_converter.h"
+#include "ImuTypes.h"
+#include "orb_slam3_pybind.h"
 #include "sophus/se3.hpp"
 
 namespace py = pybind11;
 using namespace py::literals;
-
 namespace ORB_SLAM3 {
 
 PYBIND11_MODULE(orb_slam_pybind, m) {
-  //NDArrayConverter::init_numpy();
   py::class_<System> system(m, "System");
   system
       .def(py::init<const std::string &, const std::string &,
@@ -43,45 +46,46 @@ PYBIND11_MODULE(orb_slam_pybind, m) {
            "filename"_a)
       .def(
           "TrackRGBD",
-          [](System &self, const std::string& rgb_filename,
-             const std::string& depth_filename, const double& timestamp) {
-          auto im = cv::imread(rgb_filename, cv::IMREAD_UNCHANGED);
-          auto depth = cv::imread(depth_filename, cv::IMREAD_UNCHANGED);
-        
-            std::cout << "depth image shape,type, channels, elemsize is: "<<
-            depth.size()<<std::endl; std::cout << depth.type()<<std::endl;
-            std::cout << depth.channels()<<std::endl;
-            std::cout << depth.elemSize()<<std::endl;
-          //cv::imshow("depth",depth);
-          //cv::waitKey(0);
-          return self.TrackRGBD(im, depth, timestamp).matrix();
+          [](System &self, py::array &image, py::array &depth,
+             const double &timestamp) {
+            cv::Mat im = py_array_to_mat(image);
+            cv::Mat depthmap = py_array_to_mat(depth);
+            return self.TrackRGBD(im, depthmap, timestamp).matrix();
           },
-          "im_filename"_a, "depthmap"_a, "timestamp"_a)
-      //.def(
-      //"TrackRGBD",
-      //[](System &self, py::array_t<uint8_t> &image,
-      // py::array_t<uint8_t> &depth, const double &timestamp) {
-      // NDArrayConverter::toMat(PyObject *o, cv::Mat &m)
-      // https://stackoverflow.com/questions/60917800/how-to-get-the-opencv-image-from-python-and-use-it-in-c-in-pybind11
-      // cv::Mat im(image.shape(0), image.shape(1),
-      // CV_MAKE_TYPE(CV_8U, image.shape(2)),
-      // const_cast<uint8_t *>(image.data()), image.strides(0));
-      // cv::Mat depthmap(depth.shape(0),depth.shape(1),CV_8U,
-      // const_cast<uint8_t *>(depth.data()), depth.strides(0));
+          "im"_a, "depthmap"_a, "timestamp"_a)
 
-      // cv::imshow("depth",depthmap);
-      // cv::waitKey(0);
+      .def(
+          "TrackRGBD",
+          [](System &self, py::array &image, py::array &depth,
+             const double &timestamp, py::array_t<double> vImuMeas,
+             std::string filename) {
+            std::vector<IMU::Point> vector_imu{};
+            if (!vImuMeas.is_none()) {
+              vector_imu = py_array_to_vector_imu_points(vImuMeas);
+            }
 
-      // std::cout << "color image shape, type, channels, elemsize is: "<<
-      // im.size()<<std::endl; std::cout<< im.type()<<std::endl; std::cout  <<
-      // im.channels()<<std::endl; std::cout << im.elemSize() <<std::endl;
-      // std::cout << "depth image shape,type, channels, elemsize is: "<<
-      // depthmap.size()<<std::endl; std::cout << depthmap.type()<<std::endl;
-      // std::cout << depthmap.channels()<<std::endl;
-      // std::cout << depthmap.elemSize()<<std::endl;
-      ////return self.TrackRGBD(im, depthmap, timestamp).matrix();
-      //},
-      //"im"_a, "depthmap"_a, "timestamp"_a)
+            cv::Mat im = py_array_to_mat(image);
+            cv::Mat depthmap = py_array_to_mat(depth);
+            return self.TrackRGBD(im, depthmap, timestamp, vector_imu, filename)
+                .matrix();
+          },
+          "im"_a, "depthmap"_a, "timestamp"_a,
+          "vImuMeas"_a = py::array_t<double>(), "filename"_a = "")
+
+      .def(
+          "TrackRGBD",
+          [](System &self, py::array &image, py::array &depth,
+             const double &timestamp, std::string filename) {
+            std::vector<IMU::Point> vector_imu{};
+
+            cv::Mat im = py_array_to_mat(image);
+            cv::Mat depthmap = py_array_to_mat(depth);
+
+            return self.TrackRGBD(im, depthmap, timestamp, vector_imu, filename)
+                .matrix();
+          },
+          "im"_a, "depthmap"_a, "timestamp"_a, "filename"_a)
+
       .def("Shutdown", &System::Shutdown);
 
   py::enum_<System::eSensor>(system, "eSensor")
@@ -91,6 +95,12 @@ PYBIND11_MODULE(orb_slam_pybind, m) {
       .value("IMU_MONOCULAR", System::eSensor::IMU_MONOCULAR)
       .value("IMU_STEREO", System::eSensor::IMU_STEREO)
       .value("IMU_RGBD", System::eSensor::IMU_RGBD);
-  //.export_values();
+
+  py::class_<IMU::Point> imu_point(m, "Point");
+  imu_point.def(
+      py::init<const float &, const float &, const float &, const float &,
+               const float &, const float &, const double &>(),
+      "acc_x"_a, "acc_y"_a, "acc_z"_a, "ang_vel_x"_a, "ang_vel_y"_a,
+      "ang_vel_z"_a, "timestamp"_a);
 }
 }  // namespace ORB_SLAM3
